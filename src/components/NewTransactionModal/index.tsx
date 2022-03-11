67030ef17e3010fd1fff323a1857043a179730a3
Mateus Correia <mateus.correia@outlook.com>
2022-03-11 06:27:11 -0300

    commit about the changes in the new transaction feature

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,9 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
-import { useState } from 'react';
 
 interface NewTransactionModalPros {
     isOpen: boolean;
@@ -11,7 +11,21 @@ interface NewTransactionModalPros {
 }
 
 export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionModalPros) {
-   const [type, setType] = useState('deposit');
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState (0);
+    const [category, setCategory] = useState ('');
+    const [type, setType] = useState('deposit');
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+
+        console.log({
+            title,
+            value, 
+            category,
+            type,
+        })
+    }
    
     return (
         <Modal 
@@ -30,16 +44,20 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
             </button>
 
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
             
                 <input
-                    placeholder="Titulo"            
+                    placeholder="Titulo"
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}         
                 />
 
                 <input
                     type="number"
                     placeholder="Valor"
+                    value={value}
+                    onChange={event => setValue(Number(event.target.value))}  
                 />
 
                 <TransactionTypeContainer>
@@ -47,6 +65,7 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
                     type="button"
                     onClick={() => { setType('deposit'); }}
                     isActive={type === 'deposit'}
+                    activeColor="green"
                     >
                      <img src={incomeImg} alt="Entrada" />
                      <span>Entrada</span>
@@ -56,6 +75,7 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
                     type="button"
                     onClick={() => { setType('withdraw'); }}
                     isActive={type === 'withdraw'}
+                    activeColor="red"
                     >
                      <img src={outcomeImg} alt="Saída" />
                      <span>Saída</span>
@@ -64,6 +84,8 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
 
                 <input
                     placeholder="Categoria"
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}  
                 />
 
                 <button type="submit">
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -56,12 +56,25 @@ export const TransactionTypeContainer = styled.div`
     gap: 0.5rem;
 `;
 
-export const RadioBox = styled.button`
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D'
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
-    background: transparent;
+    background: ${(props) => props.isActive 
+        ? transparentize(0.9, colors[props.activeColor])
+        : 'transparent'
+    };
 
     display: flex;
     align-items: center;
